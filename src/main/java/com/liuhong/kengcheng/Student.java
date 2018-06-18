2fc856351a7ea0f5ee959a9702a0262cba08d865
WWW <WWWW@QQ.COM>
2018-06-18 20:02:28 +0800

    2二五五点

---
+++ src/main/java/com/liuhong/kengcheng/Student.java
@@ -0,0 +1,39 @@
+package com.liuhong.kengcheng;
+
+import java.io.File;
+import java.io.Serializable;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+public class Student implements Serializable {
+	private static final long serialVersionUID = 8418649985978916033L;
+	
+	public static void main(String[] args) {
+		
+		File file = null;
+		SAXReader reader = null;
+		
+		try{
+			file = new File("F:/TomCat/apache-maven-3.5.3-bin/apache-maven-3.5.3/conf/settings.xml");
+			
+		    reader = new SAXReader();
+			Document document =  reader.read(file);
+			
+			Element root = document.getRootElement();
+			
+			
+			
+		}catch(Exception e){
+			
+		} finally{
+			
+		}
+		
+		
+		
+		
+	}
+
+}
